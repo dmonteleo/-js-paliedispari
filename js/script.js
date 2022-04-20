7e49b6522667bee0f78b441a5898c9927bcb1f04
dmonteleo <dmonteleo@gmail.com>
2022-04-20 17:44:07 +0200

    function without reverse

--- js/script.js
+++ js/script.js
@@ -1,24 +1,43 @@
-const word = prompt("inserisci una parola");
-const arrayWord = word.split("");
-const arrayWordCopy = word.split("");
-console.log(arrayWord);
 
-const arrayInverse = arrayWordCopy.reverse();
-console.log(arrayInverse);
+// const word = prompt("inserisci una parola");
+// const arrayWord = word.split("");
+// const arrayWordCopy = word.split("");
+// console.log(arrayWord);
 
-let palindromo = true;
+// const arrayInverse = arrayWordCopy.reverse();
+// console.log(arrayInverse);
 
-for ( i=0; i<arrayWord.length; i++){
-  if (arrayWord[i]!==arrayInverse[i]){
-    palindromo = false;
+// let palindromo = true;
+
+// for ( i=0; i<arrayWord.length; i++){
+//   if (arrayWord[i]!==arrayInverse[i]){
+//     palindromo = false;
+//   }
+// }
+
+// if (palindromo) {
+//   console.log("palindromo");
+// }
+// else {
+//   console.log("NON palindromo");
+// }
+
+// const input = document.querySelector("#input");
+// console.log(input);
+// input.innerHTML = "ciao";
+
+function isPalindrome() {
+  const word = prompt("inserisci una parola");
+  const arrayWord = word.split("");
+  const arrayInverse = [];
+  for (i=arrayWord.length-1; i>=0; i--) {
+    arrayInverse.push(arrayWord[i]);
   }
+  console.log(arrayInverse);
 }
 
-if (palindromo) {
-  console.log("palindromo");
-}
-else {
-  console.log("NON palindromo");
-}
+isPalindrome();
+
+
 
 
